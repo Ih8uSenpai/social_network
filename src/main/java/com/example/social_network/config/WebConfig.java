d8baa10c2bcef4f2bd3bdf6f0218de06e5884e1d
Ih8uSenpai <mipichikimicci33uwu@gmail.com>
2024-11-19 14:38:53 +0300

    upd

--- src/main/java/com/example/social_network/config/WebConfig.java
+++ src/main/java/com/example/social_network/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.social_network.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Фронтенд URL
+                .allowedOrigins(frontendUrl) // Фронтенд URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
--- src/main/java/com/example/social_network/config/WebSocketConfig.java
+++ src/main/java/com/example/social_network/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.social_network.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -16,11 +17,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.setApplicationDestinationPrefixes("/app");
     }
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000") // Или ваш фронтенд URL
+                .setAllowedOrigins(frontendUrl) // Используем переменную
                 .withSockJS();
     }
-
 }
+
