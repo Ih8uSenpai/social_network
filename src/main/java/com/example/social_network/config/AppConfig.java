448cb7dfd437d91e0a8b4ad59e7b2513176b1b54
Ih8uSenpai <mipichikimicci33uwu@gmail.com>
2025-06-15 03:26:28 +0300

    upd

--- src/main/java/com/example/social_network/config/AppConfig.java
+++ src/main/java/com/example/social_network/config/AppConfig.java
@@ -1,13 +1,32 @@
 package com.example.social_network.config;
 
+import chat.giga.langchain4j.GigaChatChatModel;
+import com.example.social_network.payextractor.Assistants.Assistant;
 import com.example.social_network.utils.TagExtractor;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.service.AiServices;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    private final GigaChatChatModel gigaChatChatModel;
+
+    public AppConfig(GigaChatChatModel gigaChatChatModel) {
+        this.gigaChatChatModel = gigaChatChatModel;
+    }
+
     @Bean
     public TagExtractor tagExtractor(){
         return new TagExtractor();
+    }
+
+    @Bean
+    public Assistant assistant()
+    {
+        return AiServices.builder(Assistant.class)
+            .chatLanguageModel(gigaChatChatModel)
+            .chatMemoryProvider(memoryId -> MessageWindowChatMemory.builder().maxMessages(10).build())
+            .build();
     }
 }
--- src/main/java/com/example/social_network/controllers/AiChatController.java
+++ src/main/java/com/example/social_network/controllers/AiChatController.java
@@ -4,8 +4,6 @@ package com.example.social_network.controllers;
 import chat.giga.langchain4j.GigaChatChatModel;
 import com.example.social_network.payextractor.Assistants.Assistant;
 import com.example.social_network.payextractor.Assistants.AssistantRegistry;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.service.AiServices;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,14 +18,16 @@ public class AiChatController {
 
     private final AssistantRegistry assistantRegistry;
     private final GigaChatChatModel gigaChatChatModel;
+    private final Assistant assistant;
     private static final Logger logger = LoggerFactory.getLogger(AiChatController.class);
 
     // Блокировка для предотвращения одновременного доступа к памяти одного sessionId.
     private final Map<String, ReentrantLock> userLocks = new ConcurrentHashMap<>();
 
-    public AiChatController(AssistantRegistry assistantRegistry, GigaChatChatModel gigaChatChatModel) {
+    public AiChatController(AssistantRegistry assistantRegistry, GigaChatChatModel gigaChatChatModel, Assistant assistant) {
         this.assistantRegistry = assistantRegistry;
         this.gigaChatChatModel = gigaChatChatModel;
+        this.assistant = assistant;
     }
 
 
@@ -49,10 +49,7 @@ public class AiChatController {
         lock.lock(); // Получаем блокировку для данного sessionId
         try {
             // Получаем (или создаём) экземпляр Assistant, соответствующий sessionId
-            Assistant assistant = AiServices.builder(Assistant.class)
-                    .chatLanguageModel(gigaChatChatModel)
-                    .chatMemoryProvider(memoryId -> MessageWindowChatMemory.builder().maxMessages(10).build())
-                    .build();
+
             String response = assistant.chat(sessionId, question);
             logger.info("Session: {}, Question: {}, Response: {}", sessionId, question, response);
             return response;
